fa75fb5e961e8faf0cae46e55d552b7c52ba1bbf
iorije <isai.orye@gmail.com>
2013-04-04 23:52:42 +0200

    AutoFontSize

--- FreshTemplateGenerator/Classes/GenerateHtml.cs
+++ FreshTemplateGenerator/Classes/GenerateHtml.cs
@@ -11,18 +11,20 @@ namespace FreshTemplateGenerator.Classes
         public static string GenerateDivs(int max, List<FormContainer> formContainer, string returnValue)
         {
             var index = 0;
+            var last = 0;
             foreach (var div in formContainer)
             {
-                if (div.rowSpan == max)
+                last += div.rowSpan;
+                if (last == max)
                 {
-                    var x = new Div().InjectClass(string.Format(" class='span-{0} last'", div.rowSpan));
+                    var x = new Div().InjectClass(string.Format(" class='span-{0} last textfill'", div.rowSpan));
                     x.InjectValue(div.value);
                     x.InjectAttribute(" style='background-color:#999999;margin:4px;'");
                     returnValue += x.htmlElement;
                 }
                 else
                 {
-                    var x = new Div().InjectClass(string.Format(" class='span-{0}'", div.rowSpan));
+                    var x = new Div().InjectClass(string.Format(" class='span-{0} textfill'", div.rowSpan));
                     x.InjectValue(div.value);
                     x.InjectAttribute(" style='background-color:#999999;margin:4px;'");
                     returnValue += x.htmlElement;
