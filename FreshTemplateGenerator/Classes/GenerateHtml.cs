9b5af10564c4488d52750f0596cd773f4c5cd3e4
iorije <isai.orye@gmail.com>
2013-04-11 00:06:47 +0200

    refactor

--- FreshTemplateGenerator/Classes/GenerateHtml.cs
+++ FreshTemplateGenerator/Classes/GenerateHtml.cs
@@ -24,72 +24,47 @@ namespace FreshTemplateGenerator.Classes
                 var currentNode = formContainer[i].node;
                 var tabs = GenerateTabs(currentNode);
                 var htmlTag = SetEnum(formContainer, currentNode, i);
-                if (htmlTag == HtmlTag.None)
+
+                var classToInject = string.Format(" class='span-{0}'", formContainer[i].rowSpan);
+                if ( htmlTag == HtmlTag.Close || htmlTag == HtmlTag.End && formContainer[i].rowSpan != 24)
                 {
-                    var classToInject = string.Format(" class='span-{0}'", formContainer[i].rowSpan);
-                    var htmlDiv = new Div().InjectClass(classToInject);
-                    htmlDiv.InjectValue(tabs + formContainer[i].value);
-                    htmlDiv.InjectAttribute(attribute);
-                    returnValue += htmlDiv.htmlElement.Replace("<", tabs + "<");
-                    htmlDiv = null;
+                    classToInject = string.Format(" class='span-{0} last'", formContainer[i].rowSpan);
                 }
-                else if (htmlTag == HtmlTag.Close)
+                var htmlDiv = new Div().InjectClass(classToInject);
+
+                if (htmlTag == HtmlTag.Open)
                 {
-                    var classToInject = string.Format(" class='span-{0} last'", formContainer[i].rowSpan);
-
-                    if (formContainer[i].rowSpan == 24)
-                    {
-                        classToInject = string.Format(" class='span-{0}'", formContainer[i].rowSpan);
-                    }
-
-                    var htmlDiv = new Div().InjectClass(classToInject);
-                    htmlDiv.InjectValue(tabs + formContainer[i].value);
-                    htmlDiv.InjectAttribute(attribute);
-                    returnValue += htmlDiv.htmlElement.Replace("<", tabs+ "<");
-                    tabs = GenerateTabs(currentNode - 1);
-
-                    returnValue += string.Format("\r\n{0}</div>\r\n", tabs);
-
-                    htmlDiv = null;
-                }
-                else if (htmlTag == HtmlTag.End)
-                {
-                    var classToInject = string.Format(" class='span-{0} last'", formContainer[i].rowSpan);
-
-                    if (formContainer[i].rowSpan == 24)
-                    {
-                        classToInject = string.Format(" class='span-{0}'", formContainer[i].rowSpan);
-                    }
-
-                    var htmlDiv = new Div().InjectClass(classToInject);
-                    htmlDiv.InjectValue(tabs + formContainer[i].value);
-                    htmlDiv.InjectAttribute(attribute);
-                    returnValue += htmlDiv.htmlElement.Replace("<", tabs + "<");
-                    tabs = GenerateTabs(currentNode - 1);
-
-                    for (var t = 0; t < currentNode-1; t++)
-                    {
-                        tabs = GenerateTabs(currentNode - (t + 1));
-                        returnValue += string.Format("\r\n{0}</div>\r\n", tabs);
-                    }
-
-                    htmlDiv = null;
-                }
-                else if (htmlTag == HtmlTag.Open)
-                {
-                    var classToInject = string.Format(" class='span-{0}'", formContainer[i].rowSpan);
-                    var htmlDiv = new Div().InjectClass(classToInject);
                     htmlDiv.htmlElement = htmlDiv.htmlElement.Replace("</div>", "");
-
                     if (!string.IsNullOrEmpty(formContainer[i].value))
                     {
                         htmlDiv.InjectValue(tabs + formContainer[i].value);
                     }
-
                     htmlDiv.InjectAttribute(attribute);
-                    returnValue += htmlDiv.htmlElement.Replace("<", tabs + "<");
-                    htmlDiv = null;
                 }
+                else
+                {
+                    htmlDiv.InjectValue(tabs + formContainer[i].value);
+                    htmlDiv.InjectAttribute(attribute);
+                }
+
+                returnValue += htmlDiv.htmlElement.Replace("<", tabs + "<");
+                if (htmlTag == HtmlTag.Close || htmlTag == HtmlTag.End)
+                {
+                    tabs = GenerateTabs(currentNode - 1);
+                    if (htmlTag == HtmlTag.Close)
+                    {
+                        returnValue += string.Format("\r\n{0}</div>\r\n", tabs);
+                    }
+                    else
+                    {
+                        for (var t = 0; t < currentNode-1; t++)
+                        {
+                            tabs = GenerateTabs(currentNode - (t + 1));
+                            returnValue += string.Format("\r\n{0}</div>\r\n", tabs);
+                        }
+                    }
+                }
+                htmlDiv = null;
             }
             return returnValue;
         }
